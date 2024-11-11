f045f18d431ba35afccc8f534e881065f6118b00
Shivanshu Koundal <126767653+shivanshuhere@users.noreply.github.com>
2024-11-11 20:05:36 +0530

    add : alertBar, side menu and hero img sec

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Navbar from "./components/Navbar";
-
+import AlertBar from "./components/AlertBar";
+import SideMenu from "./components/SideMenu";
+import HeroSection from "./components/HeroSection";
 function App() {
   return (
     <>
+      <AlertBar />
       <Navbar />
+      <section className="max-h-[50vh] bg-yellow-400 flex justify-between gap-3 px-10 ">
+        <SideMenu />
+        <HeroSection />
+      </section>
     </>
   );
 }
