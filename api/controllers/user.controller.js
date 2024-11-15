03310a209c7b4fff1c2fd9b1f4dd149c10aab21a
Shivanshu Koundal <126767653+shivanshuhere@users.noreply.github.com>
2024-11-15 20:51:58 +0530

    add : registerUser

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -28,6 +28,33 @@ const generateAccessToken = () => {
   );
 };
 
-const registerUser = () => {};
+const registerUser = async (req, res) => {
+  const { firstname, lastname, email, password, address } = req.body;
+
+  if ([firstname, email, password, address].includes("")) {
+    return res.status(400).json({ error: "All fields are required" });
+  }
+  try {
+    const user = await User.create({
+      firstname,
+      lastname,
+      email,
+      password,
+      address,
+    });
+    const newUser = User.findOne({ email: email }).select(
+      "-password -refreshToken"
+    );
+    if (!newUser) return res.status(400).json({ error: "User not found" });
+    return res.status(201).json({
+      message: "user created successfully",
+      user: newUser,
+    });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ message: "failed to create user", error: error.message });
+  }
+};
 
 export { registerUser };
