f9c0c1888821ae2e729ecadc1baaaf3bdb09e8d7
Shivanshu Koundal <126767653+shivanshuhere@users.noreply.github.com>
2024-11-15 20:39:21 +0530

    add : user model

---
+++ api/models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastname: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("user", userSchema);
