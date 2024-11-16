c63ebe4aecb78fe61c7a39747c0616b709d68116
Shivanshu Koundal <126767653+shivanshuhere@users.noreply.github.com>
2024-11-16 15:10:13 +0530

    add : catch

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
-import connectDB from "./db";
-import express from "express";
+import connectDB from "./db/index.js";
 import app from "./app.js";
 
 const PORT = process.env.PORT || 8000;
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
