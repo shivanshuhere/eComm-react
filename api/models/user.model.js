6d2a7bfba8624a81e010656088b25301314b357c
Shivanshu Koundal <126767653+shivanshuhere@users.noreply.github.com>
2024-11-16 15:11:25 +0530

    add : hashpass, gentokens

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,8 +1,10 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
-    name: {
+    firstname: {
       type: String,
       required: true,
       trim: true,
@@ -30,5 +32,31 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) return next();
+  this.password = bcrypt.hashSync(this.password, 10);
+  return next();
+});
+
+userSchema.methods.generateRefreshToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: "10d" }
+  );
+};
+
+userSchema.methods.generateAccessToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "1d" }
+  );
+};
 
 export const User = mongoose.model("user", userSchema);
