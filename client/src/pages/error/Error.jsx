af5cc118f620bad667fa413cbcf3dc6a9db3b419
Shivanshu <ks7876555@gmail.com>
2024-11-19 00:01:29 +0530

    add: error page

---
+++ client/src/pages/error/Error.jsx
@@ -0,0 +1,14 @@
+function Error() {
+    return (
+        <>
+            <div className="min-h-[50vh] flex flex-col items-center justify-center">
+                <h1 className="text-3xl font-bold">404 Not Found </h1>
+                <p className="text-lg">
+                    Your visited page not found. You may go home page.
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default Error;
