045f37eb2bcf17f1123fc8f5506a225c51296268
Shivanshu <ks7876555@gmail.com>
2024-11-19 00:01:52 +0530

    add : error route

--- client/src/router/Router.jsx
+++ client/src/router/Router.jsx
@@ -1,11 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home";
-import Register from "../pages/auth/Register"
+import Register from "../pages/auth/Register";
 import Login from "../pages/auth/Login";
 import Otp from "../pages/auth/Otp";
-
-
+import Error from "../pages/error/Error.jsx";
 
 const router = createBrowserRouter([
     {
@@ -14,23 +13,26 @@ const router = createBrowserRouter([
         children: [
             {
                 path: "/",
-                element: <Home />
+                element: <Home />,
             },
             {
                 path: "register",
-                element: <Register />
+                element: <Register />,
             },
             {
                 path: "login",
-                element: <Login />
+                element: <Login />,
             },
             {
                 path: "verify-otp",
-                element: <Otp />
-            }
-        ]
-
+                element: <Otp />,
+            },
+            {
+                path: "/*",
+                element: <Error />,
+            },
+        ],
     },
 ]);
 
-export default router
+export default router;
