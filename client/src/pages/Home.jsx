27334468717ebb1290a1143a64db59ecd78e24b7
Akhil Kumar <75962924+AKHILKAP124@users.noreply.github.com>
2024-11-22 23:50:28 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,26 +1,7 @@
-import { Link } from "react-router-dom"
-import Product_card from "../components/Product_card.jsx"
-import PanelCategory from "../components/PanelCategory.jsx"
-import { useState } from "react"
-
-
 const Home = (props) => {
   
   return (
-    
-    <div >
-        <Link to="register"> Akhil</Link>
-        {
-       <div>
-<Product_card   />
-<Product_card   />
 
-      
-       </div>
-} 
-    <PanelCategory textcolor='text-[white]' selected="bg-[#DB4444]"/>
-    </div>
-  )
 }
 
 export default Home
