d8bdfd6d56c6523726df7708fbac4a458eeb83a8
Shivanshu <ks7876555@gmail.com>
2024-11-23 17:06:13 +0530

    add : cart heading sec

---
+++ client/src/components/CartMainSec.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function CartMainSec() {
+    return (
+        <>
+            <ul className=" flex justify-between border rounded-md font-light px-10 py-5">
+                <li>Product</li>
+                <li>Price</li>
+                <li>Quantity</li>
+                <li>Subtotal</li>
+            </ul>
+        </>
+    );
+}
+
+export default CartMainSec;
