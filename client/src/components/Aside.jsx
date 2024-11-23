324785505057ef13caf0e0e3d62c4a35c240b24d
Shivanshu <ks7876555@gmail.com>
2024-11-23 16:27:18 +0530

    add : style

--- client/src/components/Aside.jsx
+++ client/src/components/Aside.jsx
@@ -5,20 +5,20 @@ function Aside({ headings, links }) {
         <>
             {headings?.map((heading, index) => {
                 return (
-                    <div key={Date.now() + index} className="">
+                    <ul key={Date.now() + index} className="flex flex-col">
                         <h1 className="text-2xl font-bold">{heading}</h1>
                         {links[index]?.map((link) => {
                             return (
                                 <Link
                                     key={Date.now()}
                                     to={link.link}
-                                    className=" text-lg font-semibold"
+                                    className=" text-lg font-semibold hover:text-red-300 "
                                 >
                                     {link.name}
                                 </Link>
                             );
                         })}
-                    </div>
+                    </ul>
                 );
             })}
         </>
