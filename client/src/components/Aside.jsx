da052ab66457b3b68607ca7ec7db6c3be3c5711b
Shivanshu <ks7876555@gmail.com>
2024-11-23 15:37:30 +0530

    add : aside component

--- client/src/components/Aside.jsx
+++ client/src/components/Aside.jsx
@@ -1,33 +1,27 @@
 import { Link } from "react-router-dom";
 
 function Aside({ headings, links }) {
-    // headings?.map((heading, index) => {
-    //     return (
-    //         <div key={index}>
-    //             <h1 className="text-2xl font-bold">{heading}</h1>
-    //             {links[index]?.map((link) => {
-    //                 return (
-    //                     <Link
-    //                         key={Date.now()}
-    //                         to={link.link}
-    //                         className=" text-lg font-semibold"
-    //                         >
-    //                         {link.name}
-    //                     </Link>
-    //                 );
-    //             })}
-    //         </div>
-    //     );
-    // });
-    console.log("headings", headings);
-    console.log("links", links);
     return (
         <>
             {headings?.map((heading, index) => {
-                <h1 className="text-2xl font-bold">{heading}</h1>;
+                return (
+                    <div key={Date.now() + index} className="">
+                        <h1 className="text-2xl font-bold">{heading}</h1>
+                        {links[index]?.map((link) => {
+                            return (
+                                <Link
+                                    key={Date.now()}
+                                    to={link.link}
+                                    className=" text-lg font-semibold"
+                                >
+                                    {link.name}
+                                </Link>
+                            );
+                        })}
+                    </div>
+                );
             })}
         </>
     );
 }
-
 export default Aside;
